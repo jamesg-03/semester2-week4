14fc87faceb12e8e77451e50239fc05c815f5f56
jamesg-03 <dpnv0860@leeds.ac.uk>
2026-05-07 15:38:30 +0000

    mc

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -1,7 +1,7 @@
 
 /*
-Name:
-Student ID:
+Name: James Graham
+Student ID: 202028828
 */
 
 #include <stdio.h>
@@ -10,8 +10,19 @@ Student ID:
 int main( int argc, char **argv ) {
 
     // define appropriate data to hold your answer
+    char buffer[100] = ""; 
 
-    // process the command-line data using appropriate string functions
+    // // process the command-line data using appropriate string functions
+    if (argc > 1) {
+        // copy first arg to buffer
+        strcpy(buffer, argv[1]);
+
+        // loops through args
+        for (int i = 2; i < argc; i++) {
+            strcat(buffer, "-"); 
+            strcat(buffer, argv[i]); 
+        }
+    }
 
     printf("%s\n",buffer); // use only this single print statement in your submitted code
 
